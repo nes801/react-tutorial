2fe696c2c292b3015572af78beedcedc611ead3e
nes801 <halbin801@gmail.com>
2018-04-25 01:49:34 +0900

    2プレイヤーが交互に打てるようにする

--- src/index.js
+++ src/index.js
@@ -2,28 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={()=>
+//         this.props.onClick()
+//       }>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      squares:Array(9).fill(null),
+      xIsNext:true,
+    };
+  }
+  handleClick(i){
+    const sques = this.state.squares.slice();
+    sques[i]=this.state.xIsNext ? 'X':'O';
+    this.setState({
+      squares:sques,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    /*Squareに値を渡す*/
+    /*キホン的になんでもカッコで囲ってあげる文化*/
+    return (
+            <Square
+              value={this.state.squares[i]}
+              onClick={()=>this.handleClick(i)}
+            />
+          );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+
+    (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
+          {/*Squareを呼び出してあげる*/}
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
